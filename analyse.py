2d9ad9ade5fbee4a39cbe3e5e8bf52a2392d6a54
Piotr Yartsev <piotr_1997@live.se>
2022-06-28 18:03:53 +0200

    made it more universall, can now iterato over all tables

--- analyse.py
+++ analyse.py
@@ -7,42 +7,46 @@ import sqlite3 as sl
 
 con = sl.connect('duplicate_data.db')
 
+for row in con.execute('SELECT name FROM sqlite_master WHERE type = "table" ORDER BY name').fetchall():
+    if row[0] == 'sqlite_sequence':
+        pass
+    else:
+        print(row[0])
 
+        add_file_number=[]
 
+        with con:
+            con.execute("""
+            ALTER TABLE USER
+                DROP COLUMN file_number;""")
+        with con:
+            con.execute("""
+            DELETE FROM {}
+                WHERE file IS NULL;""".format(row[0]))
 
-add_file_number=[]
-with con:
-     con.execute("""
-     ALTER TABLE USER
-        DROP COLUMN file_number;""")
+        with con:
+            con.execute("""
+            ALTER TABLE {}
+                ADD file_number INTEGER;""".format(row[0]))
+            
 
-with con:
-    con.execute("""
-    DELETE FROM USER
-        WHERE file IS NULL;""")
-
-with con:
-    con.execute("""
-    ALTER TABLE USER
-        ADD file_number INTEGER;""")
-     
-
-data = con.execute("SELECT file FROM USER")
-print(type(data))
-for row in data:
-    address_to_change=row[0].split("_")[3].replace("run","")
-    add_file_number.append(address_to_change)
-
-add_file_number_2=[]
-for a in add_file_number:
-    add_file_number_2.append((a))
-print(add_file_number_2)
-
-
-
-
-with con:
-    for n in range(len(add_file_number_2)):
+        data = con.execute("SELECT file FROM {}".format(row[0]))
         
-        sql="update USER set file_number=({}) where id={}".format(add_file_number_2[n],n+1)
-        con.execute(sql)
+        for rows in data:
+            address_to_change=rows[0].split("_")[3].replace("run","")
+            add_file_number.append(address_to_change)
+
+        add_file_number_2=[]
+        for a in add_file_number:
+            add_file_number_2.append((a))
+        
+
+
+
+
+        with con:
+            
+            for n in range(len(add_file_number_2)):
+                
+                sql="update {} set file_number=({}) where id={};".format(row[0],add_file_number_2[n],n+1)
+                con.execute(sql)
--- plot.py
+++ plot.py
@@ -1,3 +1,4 @@
+from tokenize import Number
 from matplotlib.axis import YAxis
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,79 +7,86 @@ from datetime import datetime
 
 import sqlite3 as sl
 
+from sqlalchemy import column
+
 con = sl.connect('duplicate_data.db')
-
-max_file_number = con.execute("""
-SELECT MAX(file_number) FROM USER;""").fetchone()[0]
-#print(max_file_number)
-
-
-
-time_1_list=[]
-time_2_list=[]
-time_no_problem=[]
-position=list(range(1,max_file_number+1))
-for number in position:
-    #print(int(number))
-    creation_time = con.execute("""
-    SELECT JobSubmissionTime FROM USER WHERE file_number LIKE ?;""", (int(number),)).fetchall()
-    
-    if len(creation_time)>1:
-        time1=creation_time[0][0]
-        time2=creation_time[1][0]
-        if time1>time2:
-            time_1_list.append(time2)
-            time_2_list.append(time1)
-        else:
-            time_1_list.append(time1)
-            time_2_list.append(time2)
+for row in con.execute('SELECT name FROM sqlite_master WHERE type = "table" ORDER BY name').fetchall():
+    if row[0] == 'sqlite_sequence':
+        pass
     else:
-        time_no_problem.append(creation_time[0][0])
-        
-creation_time_place_lund=con.execute("""
-SELECT JobSubmissionTime,file_number FROM USER WHERE ComputingElement LIKE '%lunarc%';""", ()).fetchall()
+        print(row[0])
+        max_file_number = con.execute("""
+        SELECT MAX(file_number) FROM {};""".format(row[0])).fetchone()[0]
+        min_file_number = con.execute("""
+        SELECT MIN(file_number) FROM {};""".format(row[0])).fetchone()[0]
+        #print(max_file_number)
+        position=list(range(min_file_number,max_file_number+1))
+        postion_duplicate=[]
+        position_regular=[]
+        def get_data(column):
+            time_1_list=[]
+            time_2_list=[]
+            time_no_problem=[]
+            
+            for number in position:
+                #print(int(number))
+                creation_time = con.execute("""
+                SELECT {} FROM {} WHERE file_number LIKE ?;""".format(column,row[0]), (int(number),)).fetchall()
+                #print(creation_time)
+                if len(creation_time)==0:
+                    pass
+                else:
+                    if len(creation_time)>1:
+                       # print(creation_time)
+                        time1=creation_time[0][0]
+                        time2=creation_time[1][0]
+                        postion_duplicate.append(number)
+                        if time1>time2:
+                            time_1_list.append(time2)
+                            time_2_list.append(time1)
+                        else:
+                            time_1_list.append(time1)
+                            time_2_list.append(time2)
+                    else:
+                        #print(number)
+                        position_regular.append(number)
+                        time_no_problem.append(creation_time[0][0])
+            return time_1_list,time_2_list,time_no_problem
+
+        def get_loacation_data(column,location):
+            creation_time_place=con.execute("""
+            SELECT {},file_number FROM {} WHERE ComputingElement LIKE '%{}%';""".format(column,row[0],location), ()).fetchall()
 
 
-creation_time_place_lund.sort(key=lambda x: x[1])
-creation_time_place_list_lund=[]
-creation_time_place_number_lund_list=[]
-print(creation_time_place_lund[:5])
-for n in range(len(creation_time_place_lund)):
-    creation_time_place_list_lund.append(creation_time_place_lund[n][0])
-    creation_time_place_number_lund_list.append(creation_time_place_lund[n][1])
+            creation_time_place.sort(key=lambda x: x[1])
+            creation_time_place_list=[]
+            creation_time_place_number_list=[]
+            #print(creation_time_place[:5])
+            for n in range(len(creation_time_place)):
+                creation_time_place_list.append(creation_time_place[n][0])
+                creation_time_place_number_list.append(creation_time_place[n][1])
+            return creation_time_place_list,creation_time_place_number_list
 
 
-creation_time_place_slac=con.execute("""
-SELECT JobSubmissionTime,file_number FROM USER WHERE ComputingElement LIKE '%slac%';""", ()).fetchall()
+        column="FileCreationTime"
+        time_1_list,time_2_list,time_no_problem=get_data(column)
 
+        creation_time_place_list_lund, creation_time_place_number_lund_list=get_loacation_data(column,'lunarc')
+        creation_time_place_list_slac, creation_time_place_number_slac_list=get_loacation_data(column,'slac')
+        creation_time_place_list_uscb, creation_time_place_number_uscb_list=get_loacation_data(column,'ucsb')
 
-creation_time_place_slac.sort(key=lambda x: x[1])
-creation_time_place_list_slac=[]
-creation_time_place_number_slac_list=[]
-print(creation_time_place_slac[:5])
-for n in range(len(creation_time_place_slac)):
-    creation_time_place_list_slac.append(creation_time_place_slac[n][0])
-    creation_time_place_number_slac_list.append(creation_time_place_slac[n][1])
-print(creation_time_place_list_slac[:5])
-print(len(creation_time_place_list_slac))
-print(creation_time_place_number_slac_list[:5])
-print(len(creation_time_place_number_slac_list))
+        plt.plot(creation_time_place_list_lund,creation_time_place_number_lund_list,"+",label="Created at Lund", markersize=6)
+        plt.plot(creation_time_place_list_slac,creation_time_place_number_slac_list,"*",label="Created at SLAC", markersize=6)
+        plt.plot(creation_time_place_list_uscb,creation_time_place_number_uscb_list,"o",label="Created at UCSB", markersize=6)
 
-
-
-
-
-plt.plot(creation_time_place_list_lund,creation_time_place_number_lund_list,"+",label="Created at Lund", markersize=10)
-plt.plot(creation_time_place_list_slac,creation_time_place_number_slac_list,"o",label="Created at SLAC", markersize=10)
-
-plt.plot(time_1_list,position[:len(time_1_list)],".",label="Early duplicate", markersize=10)
-plt.plot(time_2_list,position[:len(time_1_list)],".",label="Later duplicate", markersize=10)
-plt.plot(time_no_problem,position[len(time_1_list):],".",label="Not a duplicate", markersize=10)
-plt.grid(linestyle='--',)
-plt.title("Submission of file for early duplicate, late duplicate and not a duplicate file",fontsize=20)
-plt.xlabel('Time',fontsize=15)
-plt.ylabel('File number',   fontsize=15)
-plt.legend(loc="upper right",fontsize=15)
-plt.ylim(0)
-#plt.xlim(8500)
-plt.show()
+        plt.plot(time_1_list,postion_duplicate,".",label="Early duplicate", markersize=7,color="black")
+        plt.plot(time_2_list,postion_duplicate,".",label="Later duplicate", markersize=7,color="red")
+        plt.plot(time_no_problem,position_regular,".",label="Not a duplicate", markersize=4,color="white")
+        #plt.grid(linestyle='--',)
+        plt.title("Submission time of file for early duplicate, late duplicate and not a duplicate file",fontsize=20)
+        plt.xlabel('Time',fontsize=15)
+        plt.ylabel('File number',   fontsize=15)
+        plt.legend(loc="upper right",fontsize=15)
+        #plt.ylim(0)
+        #plt.xlim(8500)
+        plt.show()
