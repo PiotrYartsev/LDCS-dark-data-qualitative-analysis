240b2070cdf419d2d18a85ca82a4c3c988172cbb
Piotr Yartsev <55767612+PiotrYartsev@users.noreply.github.com>
2022-10-10 01:30:01 +0200

    more plots

--- plot/plot.py
+++ plot/plot.py
@@ -9,7 +9,7 @@ import sqlite3 as sl
 from tqdm import *
 
 from sqlalchemy import column
-name='Lund_all.db'
+name='SLAC_mc20_2.db'
 con = sl.connect(name)
 for row in con.execute('SELECT name FROM sqlite_master WHERE type = "table" ORDER BY name').fetchall():
     if row[0] == 'sqlite_sequence':
