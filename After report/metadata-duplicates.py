245d929ba4c45fdb5bb9a45f9015d7533740dd64
Piotr Yartsev <piotr_1997@live.se>
2023-04-05 15:02:50 +0200

    added test stuff

---
+++ After report/metadata-duplicates.py
@@ -0,0 +1,138 @@
+import statistics
+from tqdm import *
+import sqlite3 as sl
+import threading
+import itertools 
+
+from subprocess import PIPE, Popen
+delete_all_name='Lund_GRIDFTP_all_fixed_delete_all.db'
+delete_all = sl.connect('{}'.format(delete_all_name))
+
+
+list_of_lists=[]
+
+for row in (delete_all.execute('SELECT name FROM sqlite_master WHERE type = "table" ORDER BY name').fetchall()):
+    if row[0] == 'sqlite_sequence':
+        pass
+    else:
+        file_number_dup_more_then2=delete_all.execute("SELECT file_number from {} where duplicate>2;".format(row[0])).fetchall()
+
+        print(row[0])
+        if len(file_number_dup_more_then2)>0:
+            file_number_dup_more_then2=[i[0] for i in file_number_dup_more_then2]
+
+            file_number_dup_more_then2=list(set(file_number_dup_more_then2))
+
+            #get the DataLocations for each file_number
+            for file_number in file_number_dup_more_then2:
+                data_location=delete_all.execute("SELECT file,Scope,ComputingElement,duplicate from {} where file_number={} and ComputingElement is not Null;".format(row[0],file_number)).fetchall()
+                #print(data_location)
+                if len(data_location)>1:
+                    list_of_lists.append(data_location)
+n=0
+for i in list_of_lists:
+    n=n+len(i)
+print(n)
+"""
+"""
+def runner(input):
+    for input2 in input:
+        scope=input2[1].replace(' ','')
+        file=input2[0].replace(' ','')
+        p = Popen("rucio get-metadata {}:{}".format(scope,file), shell=True, stdout=PIPE, stderr=PIPE)
+        L_1, stderr = p.communicate()
+        #print(type(L_1))
+        stderr=stderr.decode("utf-8").split("\n")
+
+        L=L_1.decode("utf-8").split("\n")  
+        if len(stderr)>1:    
+            print(stderr)
+        
+        if len(L)<2:
+            compare_dict={}
+        else:
+            compare_dict={}
+            for line in L:
+
+                line=line.replace(" ","")
+                line=line.split(":",1)
+                if len(line)>1: 
+                    #print(line)
+                    compare_dict[line[0]]=line[1]
+        for key in compare_dict:
+            if key in compare_dict2:
+                if compare_dict[key] not in compare_dict2[key]:
+                    compare_dict2[key].append(compare_dict[key])
+            else:
+                compare_dict2[key]=[compare_dict[key]]
+
+
+
+compare_dict2={}
+files_to_sheck=list_of_lists
+files_1=files_to_sheck[:len(files_to_sheck)//8]
+files_2=files_to_sheck[len(files_to_sheck)//8:len(files_to_sheck)*2//8]
+files_3=files_to_sheck[len(files_to_sheck)*2//8:len(files_to_sheck)*3//8]
+files_4=files_to_sheck[len(files_to_sheck)*3//8:len(files_to_sheck)*4//8]
+files_5=files_to_sheck[len(files_to_sheck)*4//8:len(files_to_sheck)*5//8]
+files_6=files_to_sheck[len(files_to_sheck)*5//8:len(files_to_sheck)*6//8]
+files_7=files_to_sheck[len(files_to_sheck)*6//8:len(files_to_sheck)*7//8]
+files_8=files_to_sheck[len(files_to_sheck)*7//8:]
+with tqdm(total=max(len(files_1),len(files_2),len(files_3),len(files_4),len(files_5),len(files_6),len(files_7),len(files_8))) as pbar:
+    for (a, b, c,d,e,f,g,h) in itertools.zip_longest(files_1,files_2,files_3,files_4,files_5,files_6,files_7,files_8):
+        t1 = threading.Thread(target=runner, args=(a,))
+        t2 = threading.Thread(target=runner, args=(b,))
+        t3 = threading.Thread(target=runner, args=(c,))
+        t4 = threading.Thread(target=runner, args=(d,))
+        t5 = threading.Thread(target=runner, args=(e,))
+        t6 = threading.Thread(target=runner, args=(f,))
+        t7 = threading.Thread(target=runner, args=(g,))
+        t8 = threading.Thread(target=runner, args=(h,))
+
+        if not a==None:
+            t1.start()
+        if not b==None:
+            t2.start()
+        if not c==None:
+            t3.start()
+        if not d==None:
+            t4.start()
+        if not e==None:
+            t5.start()
+        if not f==None:
+            t6.start()
+        if not g==None:
+            t7.start()
+        if not h==None:
+            t8.start()
+            
+        if not a==None:
+            t1.join()
+        if not b==None:
+            t2.join()
+        if not c==None:
+            t3.join()
+        if not d==None:
+            t4.join()
+        if not e==None:
+            t5.join()
+        if not f==None:
+            t6.join()
+        if not g==None:
+            t7.join()
+        if not h==None:
+            t8.join()
+
+        pbar.update(1)
+
+
+data={}
+
+for key in compare_dict2:
+    if len(compare_dict2[key])>1:
+        if key in data:
+            data[key]+=len(compare_dict2[key])
+        else:
+            data[key]=len(compare_dict2[key])
+
+print(data)
